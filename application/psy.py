a6dbb04a7cbe242d8e89f34b77624374ea830335
Faralaks <Faralaks@ya.ru>
2020-02-01 15:29:13 +0300

    Усовершенствован дизайн страницы психолога

--- application/psy.py
+++ application/psy.py
@@ -1,4 +1,4 @@
-from psytest_tools import get_user_by_login, get_grades_by_psy, b64enc
+from psytest_tools import get_user_by_login, get_grades_by_psy, b64enc, vprint
 from flask import render_template, redirect, url_for, session
 from application import decorators as decors
 from application import app, mongo_connect
@@ -13,6 +13,5 @@ def psy(sort_by='result'):
     users = mongo_connect.db.users
     cur_count = get_user_by_login(session['login'])['count']
     grades = get_grades_by_psy(session['login'])
-    counters = {'testee_count':users.count_documents({'status':'testee', 'created_by':session['_id'], 'pre_del':None})}
-
+    counters = {'testee_count':users.count_documents({'status':'testee', 'added_by':session['login'], 'pre_del':None})}
     return render_template('psy.html', logged=True, login=session['login'], count=cur_count, grades=grades, counters=counters, b64enc=b64enc)
