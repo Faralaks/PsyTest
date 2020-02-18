f7ce6981399eb14beb10be7f67ca39a439126df1
Faralaks <Faralaks@ya.ru>
2020-02-18 20:14:14 +0300

    Результаты теперь по-настоящему удаляются. однако сообщение о причине удаления пока никуда не сохраняется

--- application/del_result.py
+++ application/del_result.py
@@ -1,13 +1,14 @@
 from application import app
-from flask import render_template, session, request, url_for
+from flask import render_template, session, request, url_for, redirect
 from application import decorators as decors
-from psytest_tools import b64dec
+from psytest_tools import b64dec, set_result
 
 
 @app.route('/del_result/<grade>/<login>', methods=['GET', 'POST'])
 @decors.check_psy
 def del_result(grade, login):
     if request.method == 'GET':
-        return render_template('del_result.html', logged=session.get('login', False), login=session['login'], grade=b64dec(grade), testee_login=login, back_url=url_for('grade', name=grade))
+        return render_template('del_result.html', logged=session.get('login', False), login=session['login'], grade=b64dec(grade), enc_grade=grade, testee_login=login, back_url=url_for('grade', name=grade))
     elif request.method == 'POST':
-        return '<h1>Удалено</h1>'
+        set_result(login, 'Нет результата')
+        return redirect(url_for('grade', name=grade))
