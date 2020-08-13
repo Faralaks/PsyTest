4ee2cf9801e5f641f85733abe622b3b98b7416d3
Faralaks <Faralaks@ya.ru>
2020-08-13 19:14:19 +0300

    Список психологов теперь доставляется через ajax и выыодится с помощью JS

--- application/admin.py
+++ application/admin.py
@@ -1,32 +1,46 @@
-from flask import render_template, redirect, url_for, session, request
+from flask import render_template, redirect, url_for, session, request, jsonify
 from psytest_tools import get_all_psys, gen_pass, decrypt, stamp2str
 from application import decorators as decors
 from application import app
 
 
 
-@app.route('/admin')
+@app.route('/admin', methods=['GET', 'POST'])
 @decors.check_admin
 def admin():
-    psys = get_all_psys()
-    big_counters = {'psys':0, 'whole':0, 'not_yet':0, 'clear':0, 'danger':0, 'msg':0}
-    psy_and_stats = []
+    if request.method == 'GET':
+        psys = get_all_psys()
+        big_counters = {'psys':0, 'whole':0, 'not_yet':0, 'clear':0, 'danger':0, 'msg':0}
+        psy_and_stats = []
 
-    for psy in psys:
-        big_counters['psys'] += 1
-        psy_and_stats.append({'login':psy['login'], 'pas':psy['pas'] ,'create_date':psy['create_date'], 'pre_del':psy.get('pre_del'), 'ident':psy['ident'], 'tests':psy['tests'],
-                             'count':psy['count'], 'counters':{'whole':0, 'not_yet':0, 'clear':0, 'danger':0, 'msg':0}})
+        for psy in psys:
+            big_counters['psys'] += 1
+            psy_and_stats.append({'login':psy['login'], 'pas':psy['pas'] ,'create_date':psy['create_date'], 'pre_del':psy.get('pre_del'), 'ident':psy['ident'], 'tests':psy['tests'],
+                                 'count':psy['count'], 'counters':{'whole':0, 'not_yet':0, 'clear':0, 'danger':0, 'msg':0}})
 
-        for stats in psy['grades'].values():
-            big_counters['whole'] += stats.get('whole', 0)
-            psy_and_stats[-1]['counters']['whole'] += stats.get('whole', 0)
-            big_counters['not_yet'] += stats.get('not_yet', 0)
-            psy_and_stats[-1]['counters']['not_yet'] += stats.get('not_yet', 0)
-            big_counters['clear'] += stats.get('clear', 0)
-            psy_and_stats[-1]['counters']['clear'] += stats.get('clear', 0)
-            big_counters['danger'] += stats.get('danger', 0)
-            psy_and_stats[-1]['counters']['danger'] += stats.get('danger', 0)
-            big_counters['msg'] += stats.get('msg', 0)
-            psy_and_stats[-1]['counters']['msg'] += stats.get('msg', 0)
-    return render_template('admin.html', msg=request.args.get('msg'), logged=True, login=session['login'], psys=psy_and_stats, counters=big_counters,
-                           pas=gen_pass(12), dec=decrypt, t2st=stamp2str, title='Администратор')
+        return render_template('admin.html', msg=request.args.get('msg'), logged=True, login=session['login'], psys=psy_and_stats, counters=big_counters,
+                               pas=gen_pass(1), dec=decrypt, t2st=stamp2str, title='Администратор')
+
+    elif request.method == 'POST':
+        psys = get_all_psys()
+        big_counters = {'psys':0, 'whole':0, 'not_yet':0, 'clear':0, 'danger':0, 'msg':0}
+        psy_and_stats = []
+
+        for psy in psys:
+            big_counters['psys'] += 1
+            psy_and_stats.append({'login':psy['login'], 'pas':decrypt(psy['pas']) ,'create_date':stamp2str(psy['create_date']), 'pre_del':psy.get('pre_del'), 'ident':psy['ident'], 'tests':psy['tests'],
+                                 'count':psy['count'], 'counters':{'whole':0, 'not_yet':0, 'clear':0, 'danger':0, 'msg':0}})
+
+            for stats in psy['grades'].values():
+                big_counters['whole'] += stats.get('whole', 0)
+                psy_and_stats[-1]['counters']['whole'] += stats.get('whole', 0)
+                big_counters['not_yet'] += stats.get('not_yet', 0)
+                psy_and_stats[-1]['counters']['not_yet'] += stats.get('not_yet', 0)
+                big_counters['clear'] += stats.get('clear', 0)
+                psy_and_stats[-1]['counters']['clear'] += stats.get('clear', 0)
+                big_counters['danger'] += stats.get('danger', 0)
+                psy_and_stats[-1]['counters']['danger'] += stats.get('danger', 0)
+                big_counters['msg'] += stats.get('msg', 0)
+                psy_and_stats[-1]['counters']['msg'] += stats.get('msg', 0)
+        return jsonify(psy_and_stats)
+
--- application/static/admin.js
+++ application/static/admin.js
@@ -1,5 +1,30 @@
 function getPsyList() {
+    jq.post("/admin").done( function (psyList) {
+        let psyTable = jq("#psyTable");
+        for (let i = 0; i < psyList.length; i++) {
+            let stats = `
+                <span class="badge badge-light badge-pill" title="Количество испытуемых">${ psyList[i].counters.whole}</span>
+                <span class="badge badge-secondary badge-pill" title="Еще не протестировано">${ psyList[i].counters.not_yet }</span>
+                <span class="badge badge-success badge-pill" title="Вне групп риска">${ psyList[i].counters.clear }</span>
+                <span class="badge badge-danger badge-pill" title="В группах риска">${ psyList[i].counters.danger }</span>`;
+            if (psyList[i].counters.msg) {
+                stats += `<span class="badge badge-warning badge-pill" title="Сообщения об удалении">${psyList[i].counters.msg}</span>`
+            }
+            let trPsy = jq("<tr></tr>")
+                .append(jq(`<td>${psyList[i].ident}</td>`))
+                .append(jq(`<td>${psyList[i].login}</td>`))
+                .append(jq(`<td>${psyList[i].pas}</td>`))
+                .append(jq(`<td>${psyList[i].count}</td>`))
+                .append(jq(`<td>${stats}</td>`))
+                .append(jq(`<td>${psyList[i].tests}</td>`))
+                .append(jq(`<td>${psyList[i].create_date.replace(' ', '<br/>')}</td>`))
+                .append(jq(`<td>${psyList[i].deleted?psyList[i].deleted:'-'}</td>`))
+                .append(jq(`<a class="btn btn-primary" href="/psy_info/${psyList[i].login}">Подробнее</a>`));
+             psyTable.append(trPsy);
 
+        }
+    });
+    
 
 }
 
