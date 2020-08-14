2c76d44aedc8526ef50b76bdd3f2a4ee981bf04a
Faralaks <Faralaks@ya.ru>
2020-08-14 16:23:34 +0300

    В сообщении об ошибке в связи с совпадением, теперь явно указано, совпадение по логину или по идентификатору

--- application/add_psy.py
+++ application/add_psy.py
@@ -3,9 +3,12 @@ from pymongo.errors import DuplicateKeyError
 from application import decorators as decors
 from psytest_tools import add_psy as add
 from application import app
+from psytest_tools import vprint
 
 form = lambda key: request.form[key]
 form_get = lambda key, ret: request.form.get(key, ret)
+
+err_decode = {'ident':'Идентификатор', 'login':'Логин'}
 
 @app.route('/add_psy', methods=['POST'])
 @decors.check_admin
@@ -13,6 +16,7 @@ def add_psy():
     try:
         tests = [str(i) for i in range(1, 3) if form_get('t' + str(i), None) is not None]
         add(form('login'), form('password'), form('ident'), tests, form('count'), session['login'])
-    except DuplicateKeyError: return jsonify({'status':'Err', 'msg':'Такой Логин или Идентификатор уже существует'})
+    except DuplicateKeyError as err:
+        return jsonify({'status':'Err', 'msg':'Такой %s уже существует'%err_decode[list(err.details['keyValue'].keys())[0]]})
     #except: return jsonify({'status':'Err', 'msg':'Произошла неизвестная ошибка, если проблема не исчезнет, обратитесь к администратору!'})
     return jsonify({'status':'Suc', 'msg':'Психолог успешно создан'})
