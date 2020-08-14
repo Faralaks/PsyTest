82f6d2f69b78f58078badd1e585d7d31c4c263d3
Faralaks <Faralaks@ya.ru>
2020-08-14 13:22:11 +0300

    Возвращено отображение общей статистики на странице Администраторов.

--- application/admin.py
+++ application/admin.py
@@ -9,25 +9,16 @@ from application import app
 @decors.check_admin
 def admin():
     if request.method == 'GET':
-        psys = get_all_psys()
-        big_counters = {'psys':0, 'whole':0, 'not_yet':0, 'clear':0, 'danger':0, 'msg':0}
-        psy_and_stats = []
-
-        for psy in psys:
-            big_counters['psys'] += 1
-            psy_and_stats.append({'login':psy['login'], 'pas':psy['pas'] ,'create_date':psy['create_date'], 'pre_del':psy.get('pre_del'), 'ident':psy['ident'], 'tests':psy['tests'],
-                                 'count':psy['count'], 'counters':{'whole':0, 'not_yet':0, 'clear':0, 'danger':0, 'msg':0}})
-
-        return render_template('admin.html', msg=request.args.get('msg'), logged=True, login=session['login'], psys=psy_and_stats, counters=big_counters,
-                               pas=gen_pass(1), dec=decrypt, t2st=stamp2str, title='Администратор')
+        return render_template('admin.html', logged=True, login=session['login'],
+                               pas=gen_pass(1), title='Администратор')
 
     elif request.method == 'POST':
         psys = get_all_psys()
-        big_counters = {'psys':0, 'whole':0, 'not_yet':0, 'clear':0, 'danger':0, 'msg':0}
+        big_counters = {'psy_count':0, 'whole':0, 'not_yet':0, 'clear':0, 'danger':0, 'msg':0}
         psy_and_stats = []
 
         for psy in psys:
-            big_counters['psys'] += 1
+            big_counters['psy_count'] += 1
             psy_and_stats.append({'login':psy['login'], 'pas':decrypt(psy['pas']) ,'create_date':stamp2str(psy['create_date']), 'pre_del':psy.get('pre_del'), 'ident':psy['ident'], 'tests':psy['tests'],
                                  'count':psy['count'], 'counters':{'whole':0, 'not_yet':0, 'clear':0, 'danger':0, 'msg':0}})
 
@@ -42,5 +33,5 @@ def admin():
                 psy_and_stats[-1]['counters']['danger'] += stats.get('danger', 0)
                 big_counters['msg'] += stats.get('msg', 0)
                 psy_and_stats[-1]['counters']['msg'] += stats.get('msg', 0)
-        return jsonify(psy_and_stats)
+        return jsonify({'psys':psy_and_stats, 'stats':big_counters})
 
--- application/static/admin.js
+++ application/static/admin.js
@@ -1,14 +1,7 @@
 let psyList;
-let lastKey ;
+let lastKey;
+let stats;
 
-function getPsyList() {
-    jq.post("/admin").done( function (psys) {
-        psyList = psys;
-        showPsy();
-    });
-    
-
-}
 
 function showPsy(key) {
     let psyTable = jq("#psyTable");
@@ -19,37 +12,53 @@ function showPsy(key) {
         else { reverse = 1; lastKey = key; }
 
         psyList.sort(function (a, b) {
-  if (a[key] > b[key]) {
-    return reverse;
-  }
-  if (a[key] < b[key]) {
-    return -1*reverse;
-  }
-  // a должно быть равным b
-  return 0;
-});
+        if (a[key] > b[key]) { return reverse; }
+        if (a[key] < b[key]) { return -1*reverse; }
+        return 0;
+        });
     }
+    jq('#stat_psy_count').text(stats.psy_count);
+    jq('#stat_whole').text(stats.whole);
+    jq('#stat_not_yet').text(stats.not_yet);
+    jq('#stat_clear').text(stats.clear);
+    jq('#stat_danger').text(stats.danger);
+    if (stats.msg)  jq('#stat_msg').text(stats.msg);
+    else {jq('#stat_msg').parent().remove()}
+
+
 
     for (let i = 0; i < psyList.length; i++) {
-        let stats = `
+        let ownStats = `
             <span class="badge badge-light badge-pill" title="Количество испытуемых">${ psyList[i].counters.whole}</span>
             <span class="badge badge-secondary badge-pill" title="Еще не протестировано">${ psyList[i].counters.not_yet }</span>
             <span class="badge badge-success badge-pill" title="Вне групп риска">${ psyList[i].counters.clear }</span>
             <span class="badge badge-danger badge-pill" title="В группах риска">${ psyList[i].counters.danger }</span>`;
         if (psyList[i].counters.msg) {
-            stats += `<span class="badge badge-warning badge-pill" title="Сообщения об удалении">${psyList[i].counters.msg}</span>`
+            ownStats += `<span class="badge badge-warning badge-pill" title="Сообщения об удалении">${psyList[i].counters.msg}</span>`
         }
+        let preDel;
+        if (psyList[i].pre_del) { preDel = (psyList[i].pre_del - (Date.now() / 1000 | 0))/3600 | 0}
+        else { preDel = '-' }
         let trPsy = jq("<tr></tr>")
             .append(jq(`<td>${psyList[i].ident}</td>`))
             .append(jq(`<td>${psyList[i].login}</td>`))
             .append(jq(`<td>${psyList[i].pas}</td>`))
             .append(jq(`<td>${psyList[i].count}</td>`))
-            .append(jq(`<td>${stats}</td>`))
+            .append(jq(`<td>${ownStats}</td>`))
             .append(jq(`<td>${psyList[i].tests}</td>`))
-            .append(jq(`<td>${psyList[i].create_date.replace(' ', '<br/>')}</td>`))
-            .append(jq(`<td>${psyList[i].deleted?psyList[i].deleted:'-'}</td>`))
+            .append(jq(`<td>${psyList[i].create_date.replace(' ', '<br>')}</td>`))
+            .append(jq(`<td>${preDel}</td>`))
             .append(jq(`<td><a class="btn btn-primary" href="/psy_info/${psyList[i].login}">Подробнее</a></td>`));
+        console.log(psyList[i]);
         psyTable.append(trPsy);
     }
 
+}
+
+function getPsyList() {
+    jq.post("/admin").done(function (psysAndStats) {
+        psyList = psysAndStats.psys;
+        stats = psysAndStats.stats;
+        showPsy();
+    });
 }
