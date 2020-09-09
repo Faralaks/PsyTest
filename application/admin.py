2ecf60fb2b45d244fba7fa98bb0f2e2c3fda19da
Faralaks <Faralaks@ya.ru>
2020-09-09 19:01:55 +0300

    e,hfy kbiybq bvgjhn

--- application/admin.py
+++ application/admin.py
@@ -1,5 +1,3 @@
-import time
-
 from flask import render_template, redirect, url_for, session, request, jsonify
 from psytest_tools import get_all_psys, gen_pass, decrypt, stamp2str, fixed_jsonify
 from application import decorators as decors
